300d38dea9b18ab7120570e7b0f7dad3b9b03305
Aaron Graham <aarongraham518@gmail.com>
2020-09-10 00:15:28 -0400

    connected our cakeContainer with our redux store. Created two functions to return our state and dispatch values as props to our component

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,45 @@
 import React from 'react'
+import {connect} from 'react-redux';
+import { buyCake } from '../redux';
 
-function CakeContainer() {
+function CakeContainer(props) {
     return (
         <div>
-            <h2>Number of cakes</h2>
-            <button>Buy Cake</button>
+            <h2>Number of cakes - {props.numOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
         </div>
     )
 }
 
-export default CakeContainer
+//will act as our selector (react docs)
+//Note, this is our state
+/*
+    state from our redux store is mapped to our component props
+    so apart from any props this component was receiving, it will
+    now recieve an additional prop called numOfCakes which reflects
+    the number of cakes in the redux store
+*/  
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+//note, this is our dispatch of our action creator
+/*
+    maps our dispatch of an action creator to a prop in 
+    our component. So our component will receive a 2nd 
+    additional prop called buyCake, that will dispatch
+    the buyCake action. 
+*/
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+//connects this component to the redux store!!!
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+ )(CakeContainer);

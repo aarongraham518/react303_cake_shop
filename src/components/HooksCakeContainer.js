6e8292d5361b514356937ad9e4090b6f5e5ec0f4
Aaron Graham <aarongraham518@gmail.com>
2020-09-10 18:06:20 -0400

    added useDispatch() hook and tested onClick to apply it

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
+import { buyCake } from '../redux';
 
 function HooksCakeContainer() {
     //useSelector gives us access to our state
     const numOfCakes = useSelector(state => state.numOfCakes);
 
+    //access to dispatch
+    const dispatch = useDispatch();
+
     return (
         <div>
             <h2>Num of cakes - {numOfCakes}</h2>
-            <button>Buy cake</button>
+            <button onClick={() => dispatch(buyCake())}>Buy cake</button>
         </div>
     )
 }

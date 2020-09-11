da23d4a7ba4abffada1bbbb3abb34d166e2a6c42
Aaron Graham <aarongraham518@gmail.com>
2020-09-10 23:29:43 -0400

    added redux devtools to project, love the dispatch button provided

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 // import cakeReducer from './cake/cakeReducer';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from './rootReducer';
 
 //applyMiddleware with logger as param
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
+
+/*
+npm install --save redux-devtools-extension
+*/
